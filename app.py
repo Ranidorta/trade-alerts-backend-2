3b1ff036439e9d84de27f1015091317a6877b7ed
Ranidorta <ranier.dorta@gmail.com>
2025-03-25 22:26:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def generate_signals():
     return jsonify(results)
 
 if __name__ == '__main__':
-import os
+    import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
